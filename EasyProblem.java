1c88c9b719d434064dd8cfa7e8720c2a579fc2f0
Adarsh Sharma <sharma1901adarsh@gmail.com>
2025-02-25 10:19:11 +0530

    Create EasyProblem.java

---
+++ EasyProblem.java
@@ -0,0 +1,38 @@
+import java.sql.*;
+
+public class FetchEmployeeData {
+    public static void main(String[] args) {
+        String url = ""D:\Employee"";
+        String username = "your_username";
+        String password = "your_password";
+        String query = "SELECT EmpID, Name, Salary FROM Employee";
+
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                int empID = resultSet.getInt("EmpID");
+                String name = resultSet.getString("Name");
+                double salary = resultSet.getDouble("Salary");
+
+                System.out.println("EmpID: " + empID + ", Name: " + name + ", Salary: " + salary);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            try {
+                if (resultSet != null) resultSet.close();
+                if (statement != null) statement.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                System.out.println("Error while closing resources: " + e.getMessage());
+            }
+        }
+    }
+}
